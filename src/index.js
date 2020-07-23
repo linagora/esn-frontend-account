ef8fdfea2bc981f62498f9847a68bec648a5be45
Asma AYARI <aayari@linagora.com>
2020-07-23 16:30:22 +0100

    #10 inject identities tab in profile tabs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ require('esn-frontend-mailto-handler/src/index.js');
 require('esn-frontend-account-inbox/src/index.js')
 require('./linagora.esn.controlcenter/app/app.module.js');
 require('./linagora.esn.profile/app/app.module.js');
-require('./linagora.esn.profile/app/app.module.js');
 require('./app/app.js');
 require('./all.less');
