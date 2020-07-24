18864c5802e5edc8d1990d7ebe1db23d78e8f193
Fabien MOYON <fmoyon@linagora.com>
2020-07-24 16:20:11 +0200

    #20 Add sidebar and burger menu

--- src/linagora.esn.controlcenter/app/app.run.js
+++ src/linagora.esn.controlcenter/app/app.run.js
@@ -3,12 +3,14 @@
 
   angular.module('linagora.esn.controlcenter')
 
-    .run(function(dynamicDirectiveService, DynamicDirective) {
+    .run(function(dynamicDirectiveService, DynamicDirective, $templateCache) {
       var profileMenuControlcenter = new DynamicDirective(true, 'profile-menu-controlcenter');
       var applicationMenuHome = new dynamicDirectiveService.DynamicDirective(true, 'application-menu-home', { priority: 50 });
 
       dynamicDirectiveService.addInjection('profile-menu-controlcenter', profileMenuControlcenter);
       dynamicDirectiveService.addInjection('esn-application-menu', applicationMenuHome);
+
+      $templateCache.put('/controlcenter/app/sidebar/sidebar', require('../app/sidebar/sidebar.pug'));
     });
 
 })(angular);
--- src/linagora.esn.profile/app/app.run.js
+++ src/linagora.esn.profile/app/app.run.js
@@ -3,10 +3,12 @@
 
   angular.module('linagora.esn.profile')
 
-    .run(function(dynamicDirectiveService, DynamicDirective) {
+    .run(function(dynamicDirectiveService, DynamicDirective, $templateCache) {
       var directive = new DynamicDirective(true, 'profile-menu-profile');
 
       dynamicDirectiveService.addInjection('profile-menu-profile', directive);
+
+      $templateCache.put('/profile/app/sidebar/sidebar', require('../app/sidebar/sidebar.pug'));
     });
 
 })(angular);
