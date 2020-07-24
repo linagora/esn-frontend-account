0a5165653ba48b870e6f19e0015caf9d360f02fa
Fabien MOYON <fmoyon@linagora.com>
2020-07-24 16:20:11 +0200

    #20 Add sidebar and burger menu

--- src/linagora.esn.profile/app/app.run.js
+++ src/linagora.esn.profile/app/app.run.js
@@ -1,9 +1,12 @@
-'use strict';
-
 angular.module('linagora.esn.profile')
 
   .run(function(dynamicDirectiveService, DynamicDirective) {
     var directive = new DynamicDirective(true, 'profile-menu-profile');
 
     dynamicDirectiveService.addInjection('profile-menu-profile', directive);
-  });
+  })
+  .run(cacheTemplates);
+
+  function cacheTemplates($templateCache) {
+    $templateCache.put('/profile/app/sidebar/sidebar', require('../app/sidebar/sidebar.pug'));
+  };
